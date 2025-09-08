ac6b48ae60921af17ce0c533b750fd694a5a7432
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2025-09-07 23:04:45 -0500

    adds forms, like CommentForm, and PostVideo

---
+++ app/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+
+class CommentForm(forms.Form):
+    body = forms.CharField(
+        max_length=255,
+        widget=forms.Textarea(
+            attrs={"class": "form-control", "placeholder": "Leave a comment!"}
+        )
+    )
+
+class PostVideo(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].required = False
+    title = forms.CharField(
+        max_length=255,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Title"}
+        ),
+    )
+    thumbnail = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={"class": "form-control", "required": "False"}
+        )
+    )
+    description = forms.CharField(
+        widget=forms.Textarea(
+            attrs={"class": "form-control", "placeholder": "Body"}
+        )
+    )
+    video_file = forms.FileField(upload_to='videos/') 
