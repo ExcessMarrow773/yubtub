d8e85a925563a5322d3aac7c66d7bfd5e2285395
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2025-09-08 00:42:10 -0500

    added app things to the admin page

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from app.models import Video, Comment
 # Register your models here.
+
+class VideoAdmin(admin.ModelAdmin):
+    pass
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Video, VideoAdmin)
+admin.site.register(Comment, CommentAdmin)
