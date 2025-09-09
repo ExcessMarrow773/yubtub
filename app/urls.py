b1638b262b37215127df894af8550bfff92251a7
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2025-09-08 22:45:30 -0500

    added another admin url, because Im to lazy to type the whole thing

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 urlpatterns = [
@@ -7,4 +9,5 @@ urlpatterns = [
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
     path('signup/', views.register, name='register'),
     path('video/', views.postVideo, name='postVideo'),
-]
+    path('watch/<int:pk>', views.watchVideo, name='watch')
+ ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
