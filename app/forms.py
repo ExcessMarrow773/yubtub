8d15b0a32ba1922ecc829e90365be208331aa6f6
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2025-09-21 14:25:13 -0500

    changed my forms to use ModelForms

--- app/forms.py
+++ app/forms.py
@@ -1,32 +1,14 @@
 from django import forms
 from django.contrib.admin.widgets import AdminSplitDateTime
 from django.utils import timezone
+from app.models import Video, Comment
 
-class CommentForm(forms.Form):
-    body = forms.CharField(
-        max_length=255,
-        widget=forms.Textarea(
-            attrs={"class": "form-control", "placeholder": "Leave a comment!"}
-        )
-    )
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['body']
 
-class PostVideo(forms.Form):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-    title = forms.CharField(
-        max_length=255,
-        widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "Title"}
-        ),
-    )
-    thumbnail = forms.ImageField(
-        widget=forms.FileInput(
-            attrs={"class": "form-control", "required": "False"}
-        )
-    )
-    description = forms.CharField(
-        widget=forms.Textarea(
-            attrs={"class": "form-control", "placeholder": "Body"}
-        )
-    )
-    video = forms.FileField(required=True)
+class PostVideo(forms.ModelForm):
+    class Meta:
+        model = Video
+        fields = ['title', 'description', 'thumbnail', 'video_file']
