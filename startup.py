ed3c213cb2964940f63d74b5af2c081a57dff6fc
Atticus Walton <Atticus.falkner.walton@gmail.com>
2025-09-25 19:50:48 +0000

    added a startup.py

---
+++ startup.py
@@ -0,0 +1,12 @@
+import subprocess, time
+print("Starting application...")
+time.sleep(2)  # Wait for 2 seconds to ensure the environment is ready
+subprocess.run(['.venv/bin/python3', './setup.py'])
+print("Running migrations and starting server...")
+subprocess.run(['.venv/bin/python3', 'manage.py', 'makemigrations'])
+
+subprocess.run(['.venv/bin/python3', 'manage.py', 'collectstatic', '--noinput'])
+
+subprocess.run(['.venv/bin/python3', 'manage.py', 'migrate'])
+
+subprocess.run(['.venv/bin/python3', 'manage.py', 'runserver'])
