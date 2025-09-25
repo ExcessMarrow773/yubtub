16f62ab175650e740e832a0bffd06ccc5f4db4c1
Atticus Walton <Atticus.falkner.walton@gmail.com>
2025-09-25 19:48:06 +0000

    added setup messages

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import subprocess
-
+print("Setting up virtual environment...")
 subprocess.run(['python3', '-m', 'venv', '.venv'])
-
+print("Installing required packages...")
 subprocess.run(['.venv/bin/pip', 'install', '-r', 'requirements.txt'])
+
+print("Setup complete. To activate the virtual environment, run 'source .venv/bin/activate'.")
