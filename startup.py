27652fd9f95117ac2e423b21a7c4223242dc4eb9
Atticus Walton <Atticus.falkner.walton@gmail.com>
2025-09-25 19:52:32 +0000

    updated subprocess paths to include leading slash for consistency

--- startup.py
+++ startup.py
@@ -1,12 +1,12 @@
 import subprocess, time
 print("Starting application...\n")
 time.sleep(2)  # Wait for 2 seconds to ensure the environment is ready
-subprocess.run(['.venv/bin/python3', './setup.py'])
+subprocess.run(['./.venv/bin/python3', './setup.py'])
 print("Running migrations and starting server...\n")
-subprocess.run(['.venv/bin/python3', 'manage.py', 'makemigrations'])
+subprocess.run(['./.venv/bin/python3', 'manage.py', 'makemigrations'])
 
-subprocess.run(['.venv/bin/python3', 'manage.py', 'collectstatic', '--noinput'])
+subprocess.run(['./.venv/bin/python3', 'manage.py', 'collectstatic', '--noinput'])
 
-subprocess.run(['.venv/bin/python3', 'manage.py', 'migrate'])
+subprocess.run(['./.venv/bin/python3', 'manage.py', 'migrate'])
 
-subprocess.run(['.venv/bin/python3', 'manage.py', 'runserver'])
+subprocess.run(['./.venv/bin/python3', 'manage.py', 'runserver'])
