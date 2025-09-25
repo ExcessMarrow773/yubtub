0d177cf1bb366a1f46c0fa7075521ef62459e9ab
Atticus Walton <Atticus.falkner.walton@gmail.com>
2025-09-25 19:53:15 +0000

    i did a dumb thing, this fixes it

--- startup.py
+++ startup.py
@@ -1,7 +1,7 @@
 import subprocess, time
 print("Starting application...\n")
 time.sleep(2)  # Wait for 2 seconds to ensure the environment is ready
-subprocess.run(['./.venv/bin/python3', './setup.py'])
+subprocess.run(['python3', './setup.py'])
 print("Running migrations and starting server...\n")
 subprocess.run(['./.venv/bin/python3', 'manage.py', 'makemigrations'])
 
