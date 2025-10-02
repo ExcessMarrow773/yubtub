dc0f62c86d3665aea8b2dafee931aa079228a5f0
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2025-10-01 22:38:28 -0500

    removed the collectstaticfiles command, and added KeyboardInterrupt

--- startup.py
+++ startup.py
@@ -1,12 +1,14 @@
 import subprocess, time
-print("Starting application...\n")
-time.sleep(2)  # Wait for 2 seconds to ensure the environment is ready
-subprocess.run(['python3', './setup.py'])
-print("Running migrations and starting server...\n")
-subprocess.run(['./.venv/bin/python3', 'manage.py', 'makemigrations'])
+try:
+	print("Starting application...\n")
+	subprocess.run(['git', 'pull'])
+	time.sleep(2)  # Wait for 2 seconds to ensure the environment is ready
+	subprocess.run(['python3', './setup.py'])
+	print("Running migrations and starting server...\n")
+	subprocess.run(['./.venv/bin/python3', 'manage.py', 'makemigrations'])
 
-subprocess.run(['./.venv/bin/python3', 'manage.py', 'collectstatic', '--noinput'])
+	subprocess.run(['./.venv/bin/python3', 'manage.py', 'migrate'])
 
-subprocess.run(['./.venv/bin/python3', 'manage.py', 'migrate'])
-
-subprocess.run(['./.venv/bin/python3', 'manage.py', 'runserver', '0.0.0.0:8080'])
+	subprocess.run(['./.venv/bin/python3', 'manage.py', 'runserver', '0.0.0.0:8080'])
+except KeyboardInterrupt:
+	print('\n\nexiting program')
