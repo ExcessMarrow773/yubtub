eff8383c9e9fb4009b5b8b2f0963a7e45836ae07
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2025-10-05 23:13:21 -0500

    fixed a bug in QoL.py

--- QoL.py
+++ QoL.py
@@ -4,7 +4,7 @@ i = input('QoL script name: >')
 os = platform.system()
 match i:
 	case "cdb":
-		subprocess.run(['rm media/videos/* media/templates/*'], shell=True)
+		subprocess.run(['rm media/videos/* media/thumbnail/*'], shell=True)
 		conn = sqlite3.connect('db.sqlite3')
 		cursor = conn.cursor()
 		cursor.execute('DELETE FROM app_video')
