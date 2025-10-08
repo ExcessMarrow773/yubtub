cf7954b012451563f6ad57da02dc3e938f462302
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2025-10-07 22:48:02 -0500

    adds app_name's to urls.py

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
+app_name = "yubtub"
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
