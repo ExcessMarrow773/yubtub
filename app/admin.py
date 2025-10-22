ec5a1ae9ed520926041a2defb33b9b2c87808b82
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2025-10-21 23:56:49 -0500

    changed all the register things to this new way

--- app/admin.py
+++ app/admin.py
@@ -1,20 +1,23 @@
 from django.contrib import admin
-from app.models import Video, VideoComment, Post, PostComment
+from app.models import Video, VideoComment, Post, PostComment, BugReport
 # Register your models here.
 
+@admin.register(Video)
 class VideoAdmin(admin.ModelAdmin):
     pass
 
+@admin.register(VideoComment)
 class VideoCommentAdmin(admin.ModelAdmin):
     pass
 
+@admin.register(Post)
 class PostsAdmin(admin.ModelAdmin):
     pass
 
+@admin.register(PostComment)
 class PostCommentAdmin(admin.ModelAdmin):
     pass
 
-admin.site.register(Video, VideoAdmin)
-admin.site.register(VideoComment, VideoCommentAdmin)
-admin.site.register(Post, PostsAdmin)
-admin.site.register(PostComment, PostCommentAdmin)
+@admin.register(BugReport)
+class BugReportAdmin(admin.ModelAdmin):
+    pass
