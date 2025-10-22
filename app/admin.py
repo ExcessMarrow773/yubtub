5c42a1c82f0a5f0a7f9c440f934cdbb980d8188f
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2025-10-22 00:09:22 -0500

    adds filters to Admin things

--- app/admin.py
+++ app/admin.py
@@ -4,20 +4,35 @@ from app.models import Video, VideoComment, Post, PostComment, BugReport
 
 @admin.register(Video)
 class VideoAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'author', 'created_on', 'views', 'likes')
+    list_filter = ('created_on',)
+    date_hierarchy = 'created_on'
+    search_fields = ('title', 'author')
 
 @admin.register(VideoComment)
 class VideoCommentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('author', 'video', 'created_on')
+    list_filter = ('created_on',)
+    date_hierarchy = 'created_on'
+    search_fields = ('author', 'body')
 
 @admin.register(Post)
 class PostsAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'author', 'created_on')
+    list_filter = ('created_on',)
+    date_hierarchy = 'created_on'
+    search_fields = ('title', 'author')
 
 @admin.register(PostComment)
 class PostCommentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('author', 'post', 'created_on')
+    list_filter = ('created_on',)
+    date_hierarchy = 'created_on'
+    search_fields = ('author', 'body')
 
 @admin.register(BugReport)
 class BugReportAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'author', 'type', 'has_github_issue', 'created_on')
+    list_filter = ('has_github_issue', 'created_on', 'type')
+    date_hierarchy = 'created_on'
+    search_fields = ('title', 'author', 'body')
