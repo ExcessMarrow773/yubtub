5ec85020c80b6130ddb28e9d53f3b02cd4fee9c6
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2025-10-23 00:29:27 -0500

    changes all links and refrences to pages, to use namespacing

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
+app_name = 'app'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
