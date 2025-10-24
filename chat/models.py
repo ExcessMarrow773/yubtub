09e11b63f01ebfee7423194731f8805173599f0f
Atticus Walton <atticus.falkner.walton@gmail.com>
2025-10-24 19:56:54 +0000

    removed unused model, and changed how the message model works

--- chat/models.py
+++ chat/models.py
@@ -5,20 +5,10 @@ from yubtub import settings
 
 User = settings.AUTH_USER_MODEL
 
-class Chat(models.Model):
-	to_user = models.CharField(max_length=150)
-	by_user = models.CharField(max_length=150)
-
-	class Meta:
-		verbose_name = "chat"
-		verbose_name_plural = "chats"
-
-	def __str__(self):
-		return self.name
 
 class Message(models.Model):
-	chat = models.ForeignKey("chat.Chat", related_name='messages', on_delete=models.CASCADE)
-	sender = models.CharField(max_length=150)
+	from_user = models.ForeignKey(User, related_name="from_user", on_delete=models.CASCADE)
+	to_user = models.ForeignKey(User, related_name="to_user", on_delete=models.CASCADE)
 	body = models.TextField()
 	sent_on = models.DateTimeField(auto_now_add=True)
 
