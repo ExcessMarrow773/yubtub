3116c11c34b3820844261ca547e5c7b6ae9cdd4e
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2025-10-23 21:35:13 -0500

    adds sendMsg view and messes with formatting

--- chat/views.py
+++ chat/views.py
@@ -1,32 +1,51 @@
+from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import get_user_model
-from app.models import User
+from django.views.decorators.http import require_POST
+
+import json
 
 # Create your views here.
 User = get_user_model()
 
 def index(request):
-    user = get_object_or_404(User, username=request.user.username)
-    following = user.following.all()
+	user = get_object_or_404(User, username=request.user.username)
+	following = user.following.all()
 
-    following_names = []
-    for i in following:
-        following_names.append(i.username)
+	following_names = []
+	for i in following:
+		following_names.append(i.username)
 
-    friends = []
-    for i in following:
-        if user in i.following.all():
-            friends.append(i)
+	friends = []
+	for i in following:
+		if user in i.following.all():
+			friends.append(i)
 
-    context = {
-        "friends": friends
-    }
-    return render(request, 'chatIndex.html', context)
+	context = {
+		"friends": friends
+	}
+	return render(request, 'chatIndex.html', context)
 
 def chat(request, account):
-    user = get_object_or_404(User, username=account)
-    context = {
-        'account': user
-    }
+	user = get_object_or_404(User, username=account)
+	context = {
+		'account': user
+	}
 
-    return render(request, 'chat.html', context)
+	return render(request, 'chat.html', context)
+
+@require_POST
+def sendMsg(request):
+	if not request.user.is_authenticated:
+		return JsonResponse({'message': 'Login required'}, status=403)
+
+	try:
+		data = json.loads(request.body)
+	except json.JSONDecodeError:
+		return JsonResponse({'message': 'Invalid JSON'}, status=400)
+
+
+	msg=data.get('msg')
+	to = data.get('to')
+
+	return JsonResponse({'message': msg, 'to': to}, status=200)
