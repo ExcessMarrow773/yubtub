8308dc270014cbf7d58e71db5e2703990a00585e
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2025-10-24 22:23:12 -0500

    adds admin view

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from chat.models import Message
 # Register your models here.
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('from_user', 'to_user', 'sent_on')
+    list_filter = ('sent_on',)
+    date_hierarchy = 'sent_on'
+    search_fields = ('from_user', 'to_user', 'body')
