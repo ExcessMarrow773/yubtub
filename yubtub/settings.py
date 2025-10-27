6c24573226cfac4fdeb31052fc574ecdf342423b
Atticus Walton <Atticus.falkner.walton@gmail.com>
2025-10-27 16:11:58 +0000

    changes DEBUG things

--- yubtub/settings.py
+++ yubtub/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'Ay|nBe8~ha/,TnLroK<>x7`bXq,.&c|e3-*pt$7+e43r-jwwoqmw_^*#g47)fr$&2)!(f2*awn!%u0qr6@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = False #os.getenv("DEBUG", False)
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0', '192.168.1.199', '192.168.68.120', '192.168.68.134', 'yubtub']
 
