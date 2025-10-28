64886f715f81ac2c6bc125dd4bad6c5f9bbfefe8
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2025-10-28 07:41:51 -0500

    removed unused import from chat/models.py

--- chat/models.py
+++ chat/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.conf import settings
-from django.utils import timezone
 from datetime import datetime
 
 # Avoid importing the user model at import-time to keep migrations portable.
