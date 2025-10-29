2a24aec3802e19497e276f0a94af292ee861b828
Atticus Walton <139408126+ExcessMarrow773@users.noreply.github.com>
2025-10-29 00:44:55 -0500

    adds bot logic

--- chat/bot.py
+++ chat/bot.py
@@ -20,32 +20,24 @@ def command(body, from_user):
 	commandArgs = command.split(':')
 	commandArgsLen = len(commandArgs)
 	print(commandArgs)
-	# bind args safely so we can use guards without slicing and risking IndexError
-	arg0 = commandArgs[0] if commandArgsLen > 0 else ''
-	arg1 = commandArgs[1] if commandArgsLen > 1 else ''
-	arg2 = commandArgs[2] if commandArgsLen > 2 else ''
-	match arg0:
+	match commandArgs[0]:
 		case 'info':
 			try:
-				if commandArgsLen == 1:
+				if len(commandArgs) == 1:
 					msg = 'Command malformed\n Try again'
 					newMessage(msg, from_user)
 					return msg
-				match arg1:
-					case s if s == '':
+				
+				match commandArgs[1]:
+					case '':
 						msg = 'Info about what?'
 						newMessage(msg, from_user)
 						return msg
+					
 					case s if s.startswith('user'):
-						# parse username from the argument (expecting user"name")
+						print('thing')
 						try:
-							# support either 'user"name"' or separated ':user:"name"' where the quoted name may be arg2
-							if '"' in s:
-								user = s.split('"')[1]
-							elif arg2 and '"' in arg2:
-								user = arg2.split('"')[1]
-							else:
-								raise IndexError('username not provided')
+							user = commandArgs[1].split('"')[1]
 							userInfo = User.objects.get(username=user)
 							posts_count = Post.objects.filter(author=userInfo.username).count()
 							videos_count = Video.objects.filter(author=userInfo.username).count()
@@ -53,47 +45,44 @@ def command(body, from_user):
 								f"User ID: {userInfo.id}\n"
 								f"{userInfo.username} has made {posts_count} posts\n"
 								f"{userInfo.username} has posted {videos_count} videos")
-						except (IndexError, models.CustomUser.DoesNotExist) as e:
-							msg = f'User not found or malformed argument: {e}'
+						except models.CustomUser.DoesNotExist as e:
+							msg = f'User "{user}" not found\n Error: {e}'
+						except IndexError:
+							msg = f'Please specify a user'
 						newMessage(msg, from_user)
 						return msg
-					case s if s.startswith('post'):
+					
+					case 'post':
 						if commandArgsLen > 2:
-							# use arg2 safely
-							if arg2.startswith('from'):
-								try:
-									username = arg2.split('"')[1]
-									posts_qs = Post.objects.filter(author=username)
-									if posts_qs.exists():
-										items = [f'[{p.id}], title: {p.title}' for p in posts_qs]
-										msg = f"Posts by {username}:\n" + "\n".join(items)
-									else:
-										msg = f'No posts found for "{username}"'
-								except IndexError:
-									msg = 'Malformed from argument. Use from"username"'
-							elif arg2.startswith('id'):
-								try:
-									id = arg2.split('"')[1]
-									post = Post.objects.get(id=id)
-									msg = (f"Post {id}, posted by {post.author}\n"
-										f"The title is \"{post.title}\", the body of the post is as follows\n"
-										f"{post.body}")
-								except (IndexError, Post.DoesNotExist):
-									msg = f'Post with id not found or malformed id argument'
+							if commandArgs[2][:4] == 'from':
+								username = commandArgs[2].split('"')[1]
+								posts_qs = Post.objects.filter(author=username)
+								if posts_qs.exists():
+									items = [f'[{p.id}], title: {p.title}' for p in posts_qs]
+									msg = f"Posts by {username}:\n" + "\n".join(items)
+								else:
+									msg = f'No posts found for "{username}"'
+							elif commandArgs[2][:2] == 'id':
+								id = commandArgs[2].split('"')[1]
+								post = Post.objects.get(id=id)
+								msg = f'''Post {id}, posted by {post.author}
+								The title is "{post.title}", the body of the post is as follows
+								{post.body}'''
 							else:
 								msg = 'Make sure to say the way to search the posts'
 						else:
 							msg = "What info about posts do you want?"
 						newMessage(msg, from_user)
 						return msg
-					case s if s.startswith('video'):
-						# simple placeholder for video info; extend similarly to posts above
-						if arg2.startswith('from'):
-							msg = 'video: from handling not implemented yet'
+					
+					case 'video':
+						print(commandArgs)
+						if commandArgs[2][:4] == 'from':
+							msg = 'test'
 						else:
 							msg = "What info about videos do you want?"
-						newMessage(msg, from_user)
-						return msg
+
+
 					case _:
 						msg = 'Put the thing you want information on after the colon'
 						newMessage(msg, from_user)
@@ -101,7 +90,7 @@ def command(body, from_user):
 				
 
 			except IndexError as e:
-					newMessage('command malformed\n try again', from_user)
+					newMessage(str(e), from_user)
 					return str(e)
 
 		case _:
