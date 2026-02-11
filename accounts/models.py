e7ccf5ee14f02e8c9231431e198048c4da61fc27
Atticus Walton <Atticus.falkner.walton@gmail.com>
2026-02-11 20:38:27 +0000

    removes the @ symbol, and period from the allowed char list for usernames

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.core import validators
+from django.utils.translation import gettext_lazy as _
+
 # Create your models here.
 
+class CustomUsernameValidator(UnicodeUsernameValidator):
+    regex = r'^[\w+-]+$'  # Modify this regex to allow/disallow characters
+    message = _(
+        'Enter a valid username. This value may contain only letters, '
+        'numbers, and ./-/_ characters.'
+    )
+
 class CustomUser(AbstractUser):
+	username_validator = CustomUsernameValidator()
+
 	following = models.ManyToManyField('self', symmetrical=False, related_name='followers', blank=True)
+
+	username = models.CharField(
+        max_length=150,
+        unique=True,
+        validators=[username_validator],
+		help_text=_(
+            'Required. 150 characters or fewer. Letters, digits and +/-/_ only.'
+        ),
+    )
 
 	def follow(self, user):
 		self.following.add(user)
