a9d0beff6e8c48f2b0b1589fe4e884e23d72b650
Atticus Walton <Atticus.falkner.walton@gmail.com>
2026-02-24 19:37:58 +0000

    adds correct username formatting to bugs/ bug views

--- bugs/views.py
+++ bugs/views.py
@@ -7,6 +7,8 @@ from django.views.decorators.http import require_POST
 from bugs.forms import BugReportForm
 from bugs.models import BugReport
 
+from itertools import chain
+from operator import attrgetter
 
 from app import mail
 
@@ -44,18 +46,28 @@ def bug_report(request):
 def bug_reportIndex(request):
     bugs = BugReport.objects.order_by('created_on').order_by('-created_on').filter(resolved=False)
     resolvedBugs = BugReport.objects.order_by('created_on').filter(resolved=True)
+    
+    combined = bugs | resolvedBugs
+
+    authors = {}
+    for i in combined:
+          user = User.objects.get(id=i.author).username
+          authors[i.author] = user
 
     context = {
          'resolvedBugs': resolvedBugs,
-         'bugs': bugs
+         'bugs': bugs,
+         'authors': authors
     }
     return render(request, 'bugs/bugReportIndex.html', context)
 
 def bugView(request, pk):
     bug = get_object_or_404(BugReport, pk=pk)
+    username = User.objects.get(id=bug.author).username
 
     context = {
-        'bug': bug
+        'bug': bug,
+        'username': username
     }
 
     return render(request, 'bugs/bugView.html', context)
