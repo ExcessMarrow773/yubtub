8bd670e95d31c32f8719f0fb2ce070263a982027
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2026-03-07 15:46:26 -0600

    installs upload app

--- yubtub/settings.py
+++ yubtub/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 	'chat.apps.ChatConfig',
 	'bugs.apps.BugsConfig',
 	'jsTests.apps.JstestsConfig',
+    'uploads.apps.UploadsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
