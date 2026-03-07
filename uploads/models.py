0701d55be4e113c229e3093605ee508a41151045
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2026-03-07 15:46:16 -0600

    migrate

--- uploads/models.py
+++ uploads/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Upload(models.Model):
+	author = models.IntegerField(default=0)
+	name = models.CharField(max_length=50)
+	uploaded_on = models.DateTimeField(auto_now=True)
+	file = models.FileField(upload_to='uploads/', max_length=100)
+	downloads = models.IntegerField()
